b97b3d858e83d78f6014c227d6dcc10d60d1428c
mfvitale <mvitale86@gmail.com>
2018-01-14 15:50:31 +0100

    Added simple encode function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,44 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const alphabet = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const base = alphabet.length; // base is the length of the alphabet (62 in this case)
+
+var urls = []
+
+var id = -1;
+
+exports.encode = functions.https.onRequest((request, response) => {
+    let urlToShort = request.query.url;
+
+    id +=1;
+
+    urls[id] = {
+        url: urlToShort,
+        encoded: encode(id)
+    } 
+    
+    response.send(urls[id]);
+});
+
+// utility function to convert base 10 integer to base 58 string
+function encode(num){
+    var encoded = '';
+    while (encoded.length < 4){
+      var remainder = num % base;
+      num = Math.floor(num / base);
+      encoded = alphabet[remainder].toString() + encoded;
+    }
+    return encoded;
+  }
+
+  // utility function to convert a base 58 string to base 10 integer
+function decode(str){
+    var decoded = 0;
+    while (str){
+      var index = alphabet.indexOf(str[0]);
+      var power = str.length - 1;
+      decoded += index * (Math.pow(base, power));
+      str = str.substring(1);
+    }
+    return decoded;
+  }
