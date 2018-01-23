6eb4e6300546e51abbcb2d96744261b0098c1349
mfvitale <mvitale86@gmail.com>
2018-01-23 12:32:53 +0100

    Added rewrite

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,37 @@
 const functions = require('firebase-functions');
-
+const app = require('express')();
 const admin = require('firebase-admin');
+
+
 admin.initializeApp(functions.config().firebase);
 
 var db = admin.database();
 var ref = db.ref("/");
 
 
-exports.encode = functions.https.onRequest((request, response) => {
-    let urlToShort = request.query.url;
-
+app.get('/encode', (req, res) => {  
+    let urlToShort = req.query.url;
+    
     var newUrl = ref.push();
     
     var shortenUrl = newUrl.key.substring(newUrl.key.length-5);
 
     newUrl.set({
-      url: urlToShort,
-      encoded: shortenUrl
+        url: urlToShort,
+        encoded: shortenUrl
     });
     
-    response.send(shortenUrl);
+    res.send(shortenUrl);
 });
 
-exports.decode = functions.https.onRequest((request, response) => {
-    let encoded = request.query.encoded;
-    
+
+app.get('/decode/:shorten', (req, res) => {  
+    let encoded = req.params.shorten;
+
     ref.orderByChild("encoded").equalTo(encoded).on("child_added", function(querySnapshot){
-    
-        response.redirect(301, querySnapshot.toJSON().url);
+        
+        res.redirect(301, querySnapshot.toJSON().url);
     });
 });
+
+exports.curt = functions.https.onRequest(app);
